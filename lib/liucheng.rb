d50ffc0857a83d51be50e6ca75666dfb0c5de255
404pnf <404pnf@exmaple.org>
2014-06-17 17:00:38 +0800

    statsh

--- lib/liucheng.rb
+++ lib/liucheng.rb
@@ -1,8 +1,9 @@
+# ## 公文流程类
 class Liucheng
   include Enumerable
 
-  attr_reader :user, :msg, :states, :undecided, :approved, :status, :whose_turn, :reject_msg
-  attr_accessor :done, :whose_turn, :reject_msg
+  attr_reader :user, :msg, :states, :undecided, :approved, :status, :reject_msg
+  attr_accessor :whose_turn
   def initialize(user, msg, *liucheng)
     @user = user
     @msg = msg
@@ -30,8 +31,7 @@ class Liucheng
       @approved << @states.next
       @undecided.shift
       if @undecided.empty?
-        @status = true
-        @whose_turn = nil
+        @status, @whose_turn = true, nil
       else
         @whose_turn = @undecided.first
       end
@@ -46,31 +46,56 @@ class Liucheng
     @status == :rejected
   end
 
-  def reject(msg='我根本不care')
-      @reject_msg << @whose_turn << msg
-      @status = :rejected
+  def reject(msg = '我根本不care')
+    @reject_msg << @whose_turn << msg
+    @status = :rejected
   end
 
   def show(msg)
     case msg
     when :all
-        "就你这点儿屁事，牵动了#{@states.to_a.join(' ')}这些领导的心。\n"
+      "就你这点儿屁事，牵动了#{@states.to_a.join(' ')}这些领导的心。\n"
     when :reject
-        who, msg = @reject_msg
-        "#{who}说：#{msg}"
+      who, msg = @reject_msg
+      "#{who}说：#{msg}"
     when :succeed
-      "早就都批啦。现在才来查看呀。"
+      '早就都批啦。现在才来查看呀。'
     when :approved
       if @approved.size > 0
         "#{@approved.join(' ')}已经同意了。\n"
       else
-        "领导都很忙的，还没开始审批呢。"
+        '领导都很忙的，还没开始审批呢。'
       end
     when :undecided
       "该#{@states.peek}做决定了。\n 之后会由#{@undecided[1..-1].join(' ')}做决定。"
     else
-      "我根本搞不懂你要干什么。"
+      '我根本搞不懂你要干什么。'
     end
+  end
+
+
+  def show_waiting
+    "就你这点儿屁事，牵动了#{@states.to_a.join(' ')}这些领导的心。\n"
+  end
+  def show_reject
+    who, msg = @reject_msg
+    "#{who}说：#{msg}"
+  end
+  def show_succeed
+    '早就都批啦。现在才来查看呀。'
+  end
+  def approved
+    if @approved.size > 0
+      "#{@approved.join(' ')}已经同意了。\n"
+    else
+      show_waiting
+    end
+  end
+  def show_undecided
+    "该#{@states.peek}做决定了。\n 之后会由#{@undecided[1..-1].join(' ')}做决定。"
+  end
+  def show_error
+    '我根本搞不懂你要干什么。'
   end
 
   def current
--- tests/test_liucheng.rb
+++ tests/test_liucheng.rb
@@ -3,27 +3,26 @@ require 'nanotest'
 
 include Nanotest
 
-buy_toilet_papper = Liucheng.new('小田', '手纸没了。得买了', '小曹', '老吴', '徐总' ,'彭燕' ,'常社')
+buy_toilet_papper = Liucheng.new('小田', '手纸没了。得买了', '小曹', '老吴', '徐总' , '彭燕' , '常社')
 
 buy_book = Liucheng.new('小田', '工作中用到X想看书学学', '老吴', '徐总')
 
 # 看看初始化实例是否正常
 assert { buy_toilet_papper.user == '小田' }
 assert { buy_toilet_papper.msg == '手纸没了。得买了' }
-assert { buy_toilet_papper.states.to_a == %w(小曹 老吴 徐总 彭燕 常社)}
+assert { buy_toilet_papper.states.to_a == %w(小曹 老吴 徐总 彭燕 常社) }
 assert { buy_toilet_papper.undecided ==  %w(小曹 老吴 徐总 彭燕 常社) }
 assert { buy_toilet_papper.approved == [] }
-assert { buy_toilet_papper.status == nil }
+assert { buy_toilet_papper.status.nil? }
 assert { buy_toilet_papper.whose_turn == '小曹' }
 assert { buy_book.reject_msg == [] }
 
-
 assert { buy_book.user == '小田' }
 assert { buy_book.msg == '工作中用到X想看书学学' }
-assert { buy_book.states.to_a == %w(老吴 徐总)}
+assert { buy_book.states.to_a == %w(老吴 徐总) }
 assert { buy_book.undecided ==  %w(老吴 徐总) }
 assert { buy_book.approved == [] }
-assert { buy_book.status == nil }
+assert { buy_book.status.nil? }
 assert { buy_book.whose_turn == '老吴' }
 assert { buy_book.reject_msg == [] }
 
@@ -32,7 +31,7 @@ buy_toilet_papper.approve # 1
 assert { buy_toilet_papper.undecided ==  %w(老吴 徐总 彭燕 常社) }
 assert { buy_toilet_papper.approved == ['小曹'] }
 # p buy_toilet_papper.done
-assert { buy_toilet_papper.status == nil }
+assert { buy_toilet_papper.status.nil? }
 assert { buy_toilet_papper.whose_turn == '老吴' }
 assert { buy_book.reject_msg == [] }
 
@@ -42,7 +41,7 @@ buy_toilet_papper.approve # 3
 buy_toilet_papper.approve # 4
 assert { buy_toilet_papper.undecided ==  %w(常社) }
 assert { buy_toilet_papper.approved == %w(小曹 老吴 徐总 彭燕) }
-assert { buy_toilet_papper.status == nil }
+assert { buy_toilet_papper.status.nil? }
 # p buy_toilet_papper.whose_turn
 assert { buy_toilet_papper.whose_turn == '常社' }
 assert { buy_book.reject_msg == [] }
@@ -52,13 +51,13 @@ buy_toilet_papper.approve # 5
 assert { buy_toilet_papper.undecided ==  [] }
 assert { buy_toilet_papper.approved == %w(小曹 老吴 徐总 彭燕 常社) }
 assert { buy_toilet_papper.status == true }
-assert { buy_toilet_papper.whose_turn == nil }
+assert { buy_toilet_papper.whose_turn.nil? }
 assert { buy_book.reject_msg == [] }
 
 # 看看拒绝的流程
 buy_book.reject '多用java少瞎折腾'
 # 上来老吴就拒绝了
-assert { buy_book.whose_turn = '老吴'}
+assert { buy_book.whose_turn = '老吴' }
 assert { buy_book.reject_msg == ['老吴' , '多用java少瞎折腾'] }
 
 # 重置该请求
@@ -66,10 +65,10 @@ buy_book.reset
 # 那么信息应该和上一次创建的时候一样
 assert { buy_book.user == '小田' }
 assert { buy_book.msg == '工作中用到X想看书学学' }
-assert { buy_book.states.to_a == %w(老吴 徐总)}
+assert { buy_book.states.to_a == %w(老吴 徐总) }
 assert { buy_book.undecided ==  %w(老吴 徐总) }
 assert { buy_book.approved == [] }
-assert { buy_book.status == nil }
+assert { buy_book.status.nil? }
 assert { buy_book.whose_turn == '老吴' }
 assert { buy_book.reject_msg == [] }
 
@@ -78,7 +77,3 @@ assert { buy_book.reject_msg == [] }
 buy_book.approve
 buy_book.reject
 assert { buy_book.reject_msg == ['徐总' , '我根本不care'] }
-
-
-
-
